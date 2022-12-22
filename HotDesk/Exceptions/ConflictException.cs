413775d60d865fe9278c35129cb7ca0f4774f11c
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 05:03:19 +0100

    Creating exceptions to use them in ErrorHandlingMiddleware

---
+++ HotDesk/Exceptions/ConflictException.cs
@@ -0,0 +1,10 @@
+﻿namespace HotDesk.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ HotDesk/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace HotDesk.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
