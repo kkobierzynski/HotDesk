e0d8f1646e890afb774bd69bd82eea55386d9669
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 05:04:00 +0100

    Middleware init

---
+++ HotDesk/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using HotDesk.Exceptions;
+
+namespace HotDesk.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (NotFoundException e)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch (ConflictException e)
+            {
+                context.Response.StatusCode = 409;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch(Exception e)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Oops! Something went wrong.");
+            }
+        }
+    }
+}
