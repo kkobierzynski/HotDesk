d103c90f992705d76c0e13c8fd25b4413e7b4049
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 05:07:37 +0100

    AutoMapper

---
+++ HotDesk/HotDeskMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HotDesk.Entities;
+using HotDesk.Models;
+
+namespace HotDesk
+{
+    public class HotDeskMappingProfile : Profile
+    {
+        public HotDeskMappingProfile() 
+        {
+            CreateMap<Location, LocationDto>();
+            CreateMap<Desk, DeskDto>();
+
+            CreateMap<AddLocationDto, Location>();
+            CreateMap<AddDeskDto, Desk>();
+        }
+    }
+}
