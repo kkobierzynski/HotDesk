3ae82997201e9dad8f89571190d3b05bdcdc4052
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 04:58:22 +0100

    Init

--- HotDesk/Controllers/AccountController.cs
+++ HotDesk/Controllers/AccountController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotDesk.Models;
+using HotDesk.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotDesk.Controllers
 {
+    [ApiController]
+    [Route("api/account")]
     public class AccountController : ControllerBase
     {
-        public AccountController() 
-        {
 
+        private readonly IAccountServices _accountServices;
+
+        public AccountController(IAccountServices accountServices)
+        {
+            accountServices = _accountServices;
+        }
+
+
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] LoginUserDto dto)
+        {
+            string JwtToken = _accountServices.GenerateJwt(dto);
+            return Ok(JwtToken);
         }
     }
 }
