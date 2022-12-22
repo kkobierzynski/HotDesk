6888f72681bfb75a74512ea63b481c55d0fadeff
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 04:59:48 +0100

    Add, remove, get desk by Id

--- HotDesk/Controllers/DeskController.cs
+++ HotDesk/Controllers/DeskController.cs
@@ -1,14 +1,39 @@
-﻿using HotDesk.Entities;
+﻿using HotDesk.Models;
+using HotDesk.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotDesk.Controllers
 {
+    [Route("api/location/{id}/desk")]
+    [ApiController]
     public class DeskController : ControllerBase
     {
-        public DeskController() 
-        {
-            
+        private readonly IDeskServices _deskServices;
 
+        public DeskController(IDeskServices deskServices)
+        {
+            _deskServices = deskServices;
+        }
+
+        [HttpGet("{deskId}")]
+        public ActionResult<DeskDto> GetDesk([FromRoute] int id, [FromRoute] int deskId)
+        {
+            var deskDto = _deskServices.GetDeskById(id,deskId);
+            return Ok(deskDto);
+        }
+
+        [HttpPost]
+        public ActionResult AddDesk([FromRoute] int id, [FromBody] AddDeskDto dto)
+        {
+            int deskId = _deskServices.CreateDesk(id, dto);
+            return Created($"api/location/{id}/desk/{deskId}", null);
+        }
+
+        [HttpDelete("{deskId}")]
+        public ActionResult DeleteDesk([FromRoute] int id, [FromRoute] int deskId)
+        {
+            _deskServices.DeleteDesk(id, deskId);
+            return NoContent();
         }
     }
 }
