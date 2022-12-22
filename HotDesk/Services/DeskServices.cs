0ca5c6a7c630c8736f281c38a919b2dee87e6947
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 05:00:39 +0100

    Add, remove, get desk by Id

---
+++ HotDesk/Services/DeskServices.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using HotDesk.Entities;
+using HotDesk.Exceptions;
+using HotDesk.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotDesk.Services
+{
+    public interface IDeskServices
+    {
+        DeskDto GetDeskById(int locationId, int deskId);
+        int CreateDesk(int locationId, AddDeskDto dto);
+        void DeleteDesk(int locationId, int deskId);
+    }
+    public class DeskServices : IDeskServices
+    {
+        private readonly HotDeskDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public DeskServices(HotDeskDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public DeskDto GetDeskById(int locationId, int deskId)
+        {
+            var location = _dbContext.Locations
+                .Include(location => location.Desks)
+                .FirstOrDefault(location => location.Id == locationId);
+
+            if (location == null)
+            {
+                throw new NotFoundException("Location not found");
+            }
+
+            var desk = location.Desks.FirstOrDefault(desk => desk.Id == deskId);
+
+            if (desk == null)
+            {
+                throw new NotFoundException("Desk not found");
+            }
+
+            var deskdto = _mapper.Map<DeskDto>(desk);
+            return deskdto;
+
+        }
+
+        public int CreateDesk(int locationId, AddDeskDto dto) // add Admin authorization
+        {
+            var location = _dbContext.Locations.FirstOrDefault(location => location.Id == locationId);
+            if (location == null)
+            {
+                throw new NotFoundException("The location you want to add desks to cannot be found");
+            }
+
+            var deskDto = _mapper.Map<Desk>(dto);
+            deskDto.LocationId = location.Id;
+
+            _dbContext.Desks.Add(deskDto);
+            _dbContext.SaveChanges();
+
+            return deskDto.Id;
+        }
+
+        public void DeleteDesk(int locationId, int deskId) //add admin authorization, after creating reservation actions make exception if desk is reserved
+        {
+            var location = _dbContext.Locations
+                .Include(location => location.Desks)
+                .FirstOrDefault(location => location.Id == locationId);
+
+            if (location == null)
+            {
+                throw new NotFoundException("The location you want to remove desks from cannot be found");
+            }
+
+            var desk = location.Desks.FirstOrDefault(desk => desk.Id == deskId);
+
+            if (desk == null)
+            {
+                throw new NotFoundException("Desk not found");
+            }
+
+            //bool isReserved =
+            if (false)
+            {
+                throw new ConflictException("Cannot delele desk that is reserved");
+            }
+
+            _dbContext.Desks.Remove(desk);
+            _dbContext.SaveChanges();
+        }
+    }
+}
