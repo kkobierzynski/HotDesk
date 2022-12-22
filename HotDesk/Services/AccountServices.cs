6d443f3591032948f112cee5298afc5bdd9765e7
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 05:01:03 +0100

    Init

---
+++ HotDesk/Services/AccountServices.cs
@@ -0,0 +1,22 @@
+﻿using HotDesk.Models;
+
+namespace HotDesk.Services
+{
+    public interface IAccountServices
+    {
+        string GenerateJwt(LoginUserDto dto);
+    }
+
+    public class AccountServices : IAccountServices
+    {        
+        public AccountServices()
+        {
+
+        }
+
+        public string GenerateJwt(LoginUserDto dto)
+        {
+            return "init";
+        }
+    }
+}
