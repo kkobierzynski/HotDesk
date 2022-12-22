f4c0b31609a2493c051b37f67601761131d8358e
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-22 05:01:57 +0100

    Get all, get by Id, add, remove

---
+++ HotDesk/Controllers/LocationController.cs
@@ -0,0 +1,48 @@
+﻿using HotDesk.Entities;
+using HotDesk.Models;
+using HotDesk.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotDesk.Controllers
+{
+    [Route("api/location")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationServices _locationServices;
+        public LocationController(ILocationServices locationServices)
+        {
+            _locationServices = locationServices;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<LocationDto>> GetDesksByLocalization()
+        {
+            var locations = _locationServices.GetLocations();
+            return Ok(locations);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<LocationDto> GetLocationById([FromRoute] int id)
+        {
+            var location = _locationServices.ReturnLocationById(id);
+            return Ok(location);
+        }
+
+        [HttpPost]
+        public ActionResult AddLocation(AddLocationDto dto)
+        {
+            var locationId = _locationServices.CreateLocation(dto);
+            return Created($"api/location/{locationId}", null);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteLocation(int id)
+        {
+            _locationServices.DeleteLocation(id);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ HotDesk/Services/LocationServices.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using HotDesk.Entities;
+using HotDesk.Exceptions;
+using HotDesk.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HotDesk.Services
+{
+    public interface ILocationServices
+    {
+        IEnumerable<LocationDto> GetLocations();
+        int CreateLocation(AddLocationDto dto);
+        LocationDto ReturnLocationById(int id);
+        void DeleteLocation(int id);
+    }
+
+    public class LocationServices : ILocationServices
+    {
+        private readonly HotDeskDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public LocationServices(HotDeskDbContext dbContext, IMapper mapper) 
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+
+
+        public IEnumerable<LocationDto> GetLocations()
+        {
+            var locations = _dbContext.Locations
+                .Include(location => location.Desks)
+                .ToList();
+
+            var locationsDto = _mapper.Map<List<LocationDto>>(locations);
+
+            return locationsDto;
+        }
+
+        public LocationDto ReturnLocationById(int id)
+        {
+            var location = _dbContext.Locations
+                .Include(location => location.Desks)
+                .FirstOrDefault(location => location.Id == id);
+
+            if (location == null)
+            {
+                throw new NotFoundException("Location not found");
+            }
+
+            var locationDto = _mapper.Map<LocationDto>(location);
+
+            return locationDto;
+        }
+
+        public int CreateLocation(AddLocationDto dto) //add admin authorization
+        {
+            var location = _mapper.Map<Location>(dto);
+            _dbContext.Locations.Add(location);
+            _dbContext.SaveChanges();
+            
+            return location.Id;
+        }
+
+        public void DeleteLocation(int id) //add admin authorization
+        {
+            var location = _dbContext.Locations
+                .Include(location => location.Desks)
+                .FirstOrDefault(location => location.Id == id);
+
+            if (location == null)
+            {
+                throw new NotFoundException("Location not found");
+            }
+            if (!location.Desks.IsNullOrEmpty())
+            {
+                throw new ConflictException("Cannot delete location that contains desks");
+            }
+
+            _dbContext.Locations.Remove(location);
+            _dbContext.SaveChanges();
+        }
+    }
+}
