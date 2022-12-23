ba497f0714f0aefe06e7cf115fb2a0540a781012
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-23 06:47:01 +0100

    init, one day reservation added

---
+++ HotDesk/Controllers/ReservationController.cs
@@ -0,0 +1,26 @@
+﻿using HotDesk.Models;
+using HotDesk.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotDesk.Controllers
+{
+    [Route("api/reservation")]
+    [ApiController]
+    [Authorize]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationServices _reservationServices;
+        public ReservationController(IReservationServices reservationServices)
+        {
+            _reservationServices = reservationServices;
+        }
+
+        [HttpPost("oneday")]
+        public ActionResult OneDayReservation([FromBody] DeskOneDayBookDto dto)
+        {
+            int reservationId = _reservationServices.MakeOneDayReservation(dto);
+            return Created($"api/reservation/{reservationId}", null);
+        }
+    }
+}
---
+++ HotDesk/Services/ReservationServices.cs
@@ -0,0 +1,57 @@
+﻿using HotDesk.Entities;
+using HotDesk.Exceptions;
+using HotDesk.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotDesk.Services
+{
+    public interface IReservationServices
+    {
+        int MakeOneDayReservation(DeskOneDayBookDto dto);
+    }
+
+    public class ReservationServices : IReservationServices
+    {
+        private IUserContextAccesorService _contextAccesor;
+        private readonly HotDeskDbContext _dbContext;
+
+        public ReservationServices(IUserContextAccesorService contextAccesor, HotDeskDbContext dbContext) 
+        {
+            _contextAccesor = contextAccesor;
+            _dbContext = dbContext;
+        }
+
+        public int MakeOneDayReservation(DeskOneDayBookDto dto)
+        {
+            var location = _dbContext.Locations
+                .Include(location => location.Desks)
+                .FirstOrDefault(location => location.Id == dto.LocationId);
+
+            if (location is null)
+            {
+                throw new NotFoundException("Location not found");
+            }
+
+            var desks = location.Desks.FirstOrDefault(desk => desk.Id == dto.DeskId);
+
+            if(desks is null)
+            {
+                throw new NotFoundException("Desk not found");
+            }
+
+            var reservation = new Reservation()
+            {
+                LocationId = dto.LocationId,
+                DeskId = dto.DeskId,
+                UserId = _contextAccesor.UserId is null ? throw new ConflictException("Cannot make reservation without user id information") : (int)_contextAccesor.UserId,
+                StartDate = dto.BookDay,
+                EndDate = dto.BookDay,
+            };
+
+            _dbContext.Add(reservation);
+            _dbContext.SaveChanges();
+
+            return reservation.Id;
+        }
+    }
+}
