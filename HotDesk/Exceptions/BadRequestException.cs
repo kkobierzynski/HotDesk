42c83ec8610222ae78dff6eb813cd7994d2bbbf3
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-23 06:47:29 +0100

    New exception

---
+++ HotDesk/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿namespace HotDesk.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
