4ad23d4650c456110ef6ae9c1d9800364d6c58a4
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-23 06:53:20 +0100

    context accesor init, user and userId

---
+++ HotDesk/Services/UserContextAccesorService.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace HotDesk.Services
+{
+    public interface IUserContextAccesorService
+    {
+        ClaimsPrincipal User { get; }
+        int? UserId { get; }
+    }
+    public class UserContextAccesorService : IUserContextAccesorService
+    {
+        private readonly IHttpContextAccessor _contextAccesor;
+        public UserContextAccesorService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccesor = contextAccessor;
+        }
+
+        public ClaimsPrincipal User => _contextAccesor.HttpContext?.User;
+        public int? UserId => User is null ? null : (int?)int.Parse(User.FindFirst(claim => claim.Type == ClaimTypes.NameIdentifier).Value);
+    }
+}
