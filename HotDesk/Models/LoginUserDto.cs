7becbc2ffa7a34229a7814fe95bd59655774028d
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-23 06:50:21 +0100

    Validation attributes added

--- HotDesk/Models/LoginUserDto.cs
+++ HotDesk/Models/LoginUserDto.cs
@@ -1,8 +1,12 @@
-﻿namespace HotDesk.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotDesk.Models
 {
     public class LoginUserDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
