7434a59d949ab3fa68fbfc789f07bc6457027a65
kkobierzynski <kkamil.kobierzynski@gmail.com>
2022-12-27 21:35:36 +0100

    new models

---
+++ HotDesk/Models/ReservationDto.cs
@@ -0,0 +1,13 @@
+﻿using HotDesk.Entities;
+
+namespace HotDesk.Models
+{
+    public class ReservationDto
+    {
+        public int DeskId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int UserId { get; set; }
+        public virtual UserDto User { get; set; }
+    }
+}
---
+++ HotDesk/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotDesk.Models
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
+    }
+}
